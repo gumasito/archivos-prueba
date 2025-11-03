5b68b8a1369cf36713ab1f4f03cfde9292aa97d9
Jere's git <lauralp21.love@gmail.com>
2025-11-03 12:07:53 -0300

    Saludo interesante

--- hola.py
+++ hola.py
@@ -1,2 +1,6 @@
-nombre = input("Ingrese tu nombre")
-saludo = print(f"Hola {nombre}, como te va?")
+from datetime import date
+import locale
+locale.setlocale(locale.LC_TIME, 'es_AR.UTF-8')
+nombre = input("Ingrese tu nombre ")
+dia = date.today().strftime("%A")
+saludo = print(f"Hola {nombre}, como te va en el dia {dia}")   
