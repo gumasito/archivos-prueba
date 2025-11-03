f0402e43cce87c5e054fbf98e6302cd87868a5ef
Jere :D <lauralp21.love@gmail.com>
2025-11-03 15:21:30 -0300

    Agregando Calculadora

---
+++ calcu.py
@@ -0,0 +1,14 @@
+def calcu():
+    while True:
+        print("=" * 30)
+        print("Ingrese 0 para terminar")
+        n = input("Ingrese valores>> ")
+        if (n == "0"):
+            break
+        try:
+            r = eval(n)
+            print(f"Resultado {r}")
+        except:
+            print("Expresion incorrecta, ingrese un valor correspondiente")
+
+calcu()
